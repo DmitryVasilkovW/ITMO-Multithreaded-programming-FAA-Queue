bf067a4324428105c913f193b5720c1bac0be9e1
dmitryvasilkov <dmitryvasilkovw@gmail.com>
2025-03-17 15:31:14 +0300

    feat: lab 7

--- src/FAABasedQueue.kt
+++ src/FAABasedQueue.kt
@@ -1,20 +1,90 @@
 import java.util.concurrent.atomic.*
 
 /**
- * @author TODO: Last Name, First Name
- *
- * TODO: Copy the code from `FAABasedQueueSimplified`
- * TODO: and implement the infinite array on a linked list
- * TODO: of fixed-size `Segment`s.
+ * @author Vasilkov Dmitry
  */
 class FAABasedQueue<E> : Queue<E> {
+    private val head: AtomicReference<Segment>
+    private val tail: AtomicReference<Segment>
+    private val enqIdx = AtomicLong(0)
+    private val deqIdx = AtomicLong(0)
 
-    override fun enqueue(element: E) {
-        TODO("Implement me!")
+    init {
+        val dummy = Segment(0)
+        head = AtomicReference(dummy)
+        tail = AtomicReference(dummy)
     }
 
+    override fun enqueue(element: E) {
+        while (true) {
+            val curTail = tail.get()
+            val i = enqIdx.getAndIncrement()
+            val curSegment = findSegment(begin = curTail, id = i / SEGMENT_SIZE)
+
+            moveForward(tail, src = curTail, dest = curSegment)
+            if (cas(curSegment, i, element)) {
+                return
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        TODO("Implement me!")
+        while (true) {
+            if (!shouldTryToDequeue()) {
+                return null
+            }
+
+            val curHead = head.get()
+            val i = deqIdx.getAndIncrement()
+            val curSegment = findSegment(begin = curHead, id = i / SEGMENT_SIZE)
+
+            moveForward(head, src = curHead, dest = curSegment)
+            if (cas(curSegment, i, POISONED)) {
+                continue
+            }
+
+            return curSegment.cells.getAndSet((i % SEGMENT_SIZE).toInt(), null) as E
+        }
+    }
+
+    private fun<T> cas(segment: Segment, i: Long, element: T): Boolean {
+        return segment.cells.compareAndSet(
+            (i % SEGMENT_SIZE).toInt(),
+            null,
+            element
+        )
+    }
+
+    private fun findSegment(begin: Segment, id: Long): Segment {
+        var cur = begin
+        for (curId in begin.id..< id) {
+            val next = Segment(curId + 1)
+            cur.next.compareAndSet(null, next)
+            cur = cur.next.get()!!
+        }
+
+        return cur
+    }
+
+    private fun moveForward(
+        part: AtomicReference<Segment>,
+        src: Segment,
+        dest: Segment
+    ) {
+        if (src.id < dest.id) {
+            part.compareAndSet(src, dest)
+        }
+    }
+
+    private fun shouldTryToDequeue(): Boolean {
+        while (true) {
+            val curDeqIdx = deqIdx.get()
+            val curEnqIdx = enqIdx.get()
+            if (curDeqIdx == deqIdx.get()) {
+                return curDeqIdx < curEnqIdx
+            }
+        }
     }
 }
 
@@ -25,3 +95,4 @@ private class Segment(val id: Long) {
 
 // DO NOT CHANGE THIS CONSTANT
 private const val SEGMENT_SIZE = 2
+private val POISONED = Any()
--- src/FAABasedQueueSimplified.kt
+++ src/FAABasedQueueSimplified.kt
@@ -2,34 +2,55 @@ import java.util.concurrent.atomic.*
 import kotlin.math.*
 
 /**
- * @author TODO: Last Name, First Name
+ * @author Vasilkov Dmitry
  */
 class FAABasedQueueSimplified<E> : Queue<E> {
-    private val infiniteArray = AtomicReferenceArray<Any?>(1024) // conceptually infinite array
+    private val infiniteArray = AtomicReferenceArray<Any?>(10)
     private val enqIdx = AtomicLong(0)
     private val deqIdx = AtomicLong(0)
 
     override fun enqueue(element: E) {
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = enqIdx.get()
-        enqIdx.set(i + 1)
-        // TODO: Atomically install the element into the cell
-        // TODO: if the cell is not poisoned.
-        infiniteArray.set(i.toInt(), element)
+        while (true) {
+            val i = enqIdx.getAndIncrement()
+            if (cas(i, element)) {
+                return
+            }
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun dequeue(): E? {
-        // Is this queue empty?
-        if (enqIdx.get() <= deqIdx.get()) return null
-        // TODO: Increment the counter atomically via Fetch-and-Add.
-        // TODO: Use `getAndIncrement()` function for that.
-        val i = deqIdx.get()
-        deqIdx.set(i + 1)
-        // TODO: Try to retrieve an element if the cell contains an
-        // TODO: element, poisoning the cell if it is empty.
-        return infiniteArray.get(i.toInt()) as E
+        while (true) {
+            if (!shouldTryToDequeue()) {
+                return null
+            }
+
+            val i = deqIdx.getAndIncrement()
+            if (cas(i, POISONED)) {
+                continue
+            }
+
+            return infiniteArray.getAndSet(i.toInt(), null) as E
+        }
+    }
+
+    private fun shouldTryToDequeue(): Boolean {
+        while (true) {
+            val curDeqIdx = deqIdx.get()
+            val curEnqIdx = enqIdx.get()
+
+            if (curDeqIdx == deqIdx.get()) {
+                return curDeqIdx < curEnqIdx
+            }
+        }
+    }
+
+    private fun<T> cas(i: Long, element: T): Boolean {
+        return infiniteArray.compareAndSet(
+            i.toInt(),
+            null,
+            element
+        )
     }
 
     override fun validate() {
@@ -46,5 +67,4 @@ class FAABasedQueueSimplified<E> : Queue<E> {
     }
 }
 
-// TODO: poison cells with this value.
 private val POISONED = Any()
